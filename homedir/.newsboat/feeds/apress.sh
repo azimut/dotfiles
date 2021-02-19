f52f1d06cd5c2a88c6045c8a6d0162e77c52cfc1
azimut <azimut.github@protonmail.com>
2021-02-18 23:11:35 -0300

    newsboat apress

---
+++ homedir/.newsboat/feeds/apress.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -exuo pipefail
+
+TITLE='Apress New Releases'
+DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+URL=https://www.apress.com/la
+ID='apress:newreleases'
+
+echo "<?xml version='1.0' encoding='utf-8'?>
+<feed xmlns='http://www.w3.org/2005/Atom'>
+  <title> ${TITLE} </title>
+  <id> ${ID} </id>
+  <updated>${DATE}</updated>"
+
+torsocks curl "${URL}" \
+    | pup 'div#id3 div.row div.columns div.cms-recommendation-long ul li div.product-information json{}' \
+    | jq -r '.[] as $li | "
+<entry>
+  <title>\($li.children[0].children[0].text)</title>
+  <id>\($li.children[0].children[0].href)</id>
+  <published>'${DATE}'</published>
+  <link href=\"\($li.children[0].children[0].href)\">\($li.children[0].children[0].href)</link>
+  <author><name>\($li.children[1].text | gsub("\\s+";" ";"g"))</name></author>
+</entry>"' | sed 's;<link href="/;<link href="https://www.apress.com/;'
+
+echo
+echo "</feed>"
+
