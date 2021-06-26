cc7c780fb7f04c066b5dfe33c1aff3c003173520
azimut <azimut.github@protonmail.com>
2021-06-26 17:36:22 -0300

    apress: fix, now homepage doesn't return anything sane

--- homedir/.newsboat/feeds/apress.sh
+++ homedir/.newsboat/feeds/apress.sh
@@ -3,7 +3,7 @@ set -exuo pipefail
 
 TITLE='Apress New Releases'
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-URL=https://www.apress.com/la
+URL='https://www.apress.com/la/product-search?dnc=true&facet-pdate=pdate__onemonth&query='
 ID='apress:newreleases'
 
 echo "<?xml version='1.0' encoding='utf-8'?>
@@ -14,15 +14,15 @@ echo "<?xml version='1.0' encoding='utf-8'?>
   <updated>${DATE}</updated>"
 
 torsocks curl "${URL}" \
-    | pup 'div#id3 div.row div.columns div.cms-recommendation-long ul li div.product-information json{}' \
+    | pup 'div#result-list div.result-item json{}' \
     | jq -r '.[] as $li | "
 <entry>
-  <title>\($li.children[0].children[0].text)</title>
-  <id>\($li.children[0].children[0].href)</id>
+  <title>\($li.children[2].children[0].text)</title>
+  <id>\($li.children[0].href)</id>
   <published>'${DATE}'</published>
-  <link href=\"\($li.children[0].children[0].href)\">\($li.children[0].children[0].href)</link>
-  <author><name>\($li.children[1].text | gsub("\\s+";" ";"g"))</name></author>
+  <link href=\"\($li.children[0].href)\">\($li.children[0].href)</link>
+  <author><name>\($li.children[3].text | gsub("\\s+";" ";"g"))</name></author>
 </entry>"' | sed 's;<link href="/;<link href="https://www.apress.com/;'
 
-echo
-echo "</feed>"
+    echo
+    echo "</feed>"
