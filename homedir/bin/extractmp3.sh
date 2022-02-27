69411b03ed6a0522f103e012bcd5651409880cee
azimut <azimut.github@protonmail.com>
2022-02-27 11:25:16 -0300

    extractmp3: add middle frame instead of frame 0
    I didn't use just seconds with -ss as it was painfully slow for some
    reason, it might have worked with frames (?)

--- homedir/bin/extractmp3.sh
+++ homedir/bin/extractmp3.sh
@@ -5,7 +5,17 @@ INPUT="$1"
 test -f "${INPUT}"
 output="$(basename "${INPUT}").mp3"
 
+half() { echo "$(</dev/stdin) * 0.5" | bc -l ; }
+round(){ printf "%.0f" "$(</dev/stdin)"; }
+video_to_seconds() {
+    ffprobe -loglevel error -hide_banner -of csv=p=0 -show_entries format=duration "${1}" | half | round
+}
+seconds_to_timestamp(){
+    date -u --date=@"$(</dev/stdin)" +%H:%M:%S
+}
+
 rm -f output.jpg
-ffmpeg -hide_banner -y -i "${INPUT}" -frames:v 1 output.jpg
+ffmpeg -ss "$(video_to_seconds "${INPUT}" |seconds_to_timestamp)" \
+       -hide_banner -y -i "${INPUT}" -frames:v 1 output.jpg
 ffmpeg -hide_banner -i "${INPUT}" -i output.jpg -ac 1 -c:v mjpeg -map 0:a -map 1:v "${output}"
 rm -f output.jpg
