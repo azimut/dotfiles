94d24c181f68217a7393b39dfbdf2e4e1982580f
azimut <azimut.github@protonmail.com>
2022-03-04 19:12:44 -0300

    extractmp3: half -> div

--- homedir/bin/extractmp3.sh
+++ homedir/bin/extractmp3.sh
@@ -5,7 +5,7 @@ INPUT="$1"
 test -f "${INPUT}"
 output="$(basename "${INPUT}").mp3"
 
-half() { echo "$(</dev/stdin) * 0.5" | bc -l ; }
+div() { echo "$(</dev/stdin) / ${1}" | bc -l ; }
 round(){ printf "%.0f" "$(</dev/stdin)"; }
 video_to_seconds() {
     ffprobe -loglevel error -hide_banner -of csv=p=0 -show_entries format=duration "${1}"
@@ -15,7 +15,7 @@ seconds_to_timestamp(){
 }
 
 rm -f output.jpg
-ffmpeg -ss "$(video_to_seconds "${INPUT}" | half | round | seconds_to_timestamp)" \
+ffmpeg -ss "$(video_to_seconds "${INPUT}" | div 2 | round | seconds_to_timestamp)" \
        -hide_banner -y -i "${INPUT}" -frames:v 1 output.jpg
 test -s output.jpg
 ffmpeg -hide_banner -i "${INPUT}" -i output.jpg -ac 1 -c:v mjpeg -map 0:a -map 1:v "${output}"
