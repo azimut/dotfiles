d949d6f502aeeda8cf4a6e106109abda211210e6
azimut <azimut.github@protonmail.com>
2022-04-18 04:34:43 -0300

    mooc.sh: add yt-dlp to download moocs

---
+++ homedir/bin/mooc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Description: Downloads a playlist of a MOOC
+# - With subs
+# - With low video quality for space
+# - With the best audio (TODO: converted to mono)
+# - Name numbered
+
+set -exuo pipefail
+
+URL="${1}"
+LESS="${2:-720}"
+
+echo "${URL}" >url
+
+yt-dlp \
+	--output '%(playlist_index)03d-%(title)s[%(id)s].%(ext)s' \
+	--format 'bestvideo[height<'"${LESS}"']+bestaudio' \
+	--write-subs --sub-langs 'en.*,es.*' \
+	--continue \
+	"${URL}"
