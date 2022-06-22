341734cdcaee4fbeb9a3ade7fa581e9106efc2ea
azimut <azimut.github@protonmail.com>
2022-06-22 00:03:16 -0300

    pinp: top right

--- homedir/bin/pinp.sh
+++ homedir/bin/pinp.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# TODO: resize keeping geometry
-#       xdotool getwindowgeometry ${ID} | grep Geometry
-# TODO: place in different corner
+# TODO: option to pick a corner
 #       https://gist.github.com/andrewmcdonough/88246/81d540704fdd7b753083da7e5e718abc23b87467
-# TODO: offset toolbar size
-# TODO: visible on all windows
 
 set -exuo pipefail
 
@@ -14,7 +10,36 @@ getID() {
 		xdotool search --name picture-in-picture
 }
 
+getScreenDimensions() {
+	xrandr |
+		grep -o 'current [[:digit:]]* x [[:digit:]]*' |
+		cut -f2,4 -d' '
+}
+
+getWindowWidth() {
+	local id="${1}"
+	xdotool getwindowgeometry --shell ${id} |
+		grep WIDTH |
+		cut -f2 -d'='
+}
+
+read -r -a SCREEN < <(getScreenDimensions)
+
+MULTIPLIER="${1:-1}"
+BAR_OFFSET=20
+
 ID="$(getID)"
 
-xdotool windowsize ${ID} 200 100
-xdotool windowmove ${ID} 0 0
+SCREEN_WIDTH="${SCREEN[0]}"
+SCREEN_HEIGHT="${SCREEN[1]}"
+
+TARGET_WIDTH=$((200 * MULTIPLIER))
+TARGET_HEIGHT=$((100 * MULTIPLIER))
+
+xdotool windowsize ${ID} "${TARGET_WIDTH}" "${TARGET_HEIGHT}"
+
+EFFECTIVE_WIDTH="$(getWindowWidth ${ID})"
+
+xdotool windowmove ${ID} \
+	$((SCREEN_WIDTH - EFFECTIVE_WIDTH)) \
+	$((0 + BAR_OFFSET))
