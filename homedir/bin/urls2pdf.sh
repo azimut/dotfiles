15724ed243f6929ee69ff143fb70c4109043f317
azimut <azimut.github@protonmail.com>
2022-12-31 10:02:35 -0300

    file of urls to pdfs

---
+++ homedir/bin/urls2pdf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+fetch() {
+	chromium-browser \
+		--incognito \
+		--headless \
+		--disable-gpu \
+		--print-to-pdf \
+		"${1}"
+}
+
+[[ -s ${1} ]] || exit 1
+
+while read -r url; do
+	path="$(echo "${url}" | sed -rn 's#http[s]?://[a-zA-Z0-9.-]+/(.*)#\1#p')"
+	path="${path%/}"
+	name="$(echo "${path}" | tr '/' '-' | tr -dc 'a-zA-Z0-9-_')"
+	name=${name:-root}
+	fetch "${url}"
+	mv output.pdf "${name}".pdf
+done <"${1}"
