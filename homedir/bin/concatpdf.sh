12044f613aca16c25a0aacaa4d79d41f279292c4
azimut <azimut.github@protonmail.com>
2023-01-04 14:33:08 -0300

    concatpdf.sh: take file arguments for custom order

--- homedir/bin/concatpdf.sh
+++ homedir/bin/concatpdf.sh
@@ -5,11 +5,17 @@ set -exuo pipefail
 tp="tmp.pdf"
 td="data"
 
-for i in *.pdf; do
+if [[ $# -eq 0 ]]; then
+	files=(*.pdf)
+else
+	files=("${@}")
+fi
+
+for i in "${files[@]}"; do
 	echo "Bookmarking $i"
 	printf "BookmarkBegin\nBookmarkTitle: %s\nBookmarkLevel: 1\nBookmarkPageNumber: 1\n" "${i%.*}" >"$td"
 	pdftk "$i" update_info "$td" output "$tp"
 	mv "$tp" "$i"
 done
 
-pdftk ./*.pdf cat output myBook.pdf
+pdftk "${files[@]}" cat output myBook.pdf
