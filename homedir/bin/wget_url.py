2433f8e7ab5d43854e2f93fb3086fe9e9284becb
azimut <azimut.github@protonmail.com>
2023-01-26 19:47:24 -0300

    wget_url.py: add progress hook
    https://blog.shichao.io/2012/10/04/progress_speed_indicator_for_urlretrieve_in_python.html

--- homedir/bin/wget_url.py
+++ homedir/bin/wget_url.py
@@ -2,14 +2,30 @@
 
 import sys
 import os.path
+from time import time
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
+
+
+def progress_hook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time()
+        return
+    duration = time() - start_time
+    progress_size = int(count * block_size)
+    speed = int(progress_size / (1024*duration))
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
+                     (percent, progress_size / (1024*1024), speed, duration))
+    sys.stdout.flush()
 
 
 def download(url):
     urlpath = urlparse(url).path
     _, filename = os.path.split(urlpath)
-    urlretrieve(url, filename)
+    urlretrieve(url, filename, progress_hook)
+    print("\ndone!", filename)
 
 
 try:
