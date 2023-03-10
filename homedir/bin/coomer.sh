ebbdeefea3a1be543629567a0231a7110235f36a
azimut <azimut.github@protonmail.com>
2023-03-10 12:26:41 -0300

    update sxiv->feh

--- homedir/bin/coomer.sh
+++ homedir/bin/coomer.sh
@@ -3,18 +3,18 @@
 set -euo pipefail
 
 container_and_file() {
-    for link in ~/.coomer/*jpg ~/.coomer/*png; do
-        if [[ ! -L ${link} ]]; then
-            continue
-        fi
-        file=$(readlink ${link})
-        container=${file%/*}
-        container=${container##*/}
-        echo ${container},${file}
-    done
+	for link in ~/.coomer/*jpg ~/.coomer/*png; do
+		if [[ ! -L ${link} ]]; then
+			continue
+		fi
+		file=$(readlink ${link})
+		container=${file%/*}
+		container=${container##*/}
+		echo ${container},${file}
+	done
 }
 
-container_and_file \
-    | sort -k1,1 -t, \
-    | cut -f2 -d, \
-    | xargs sxiv
+container_and_file |
+	sort -k1,1 -t, |
+	cut -f2 -d, |
+	xargs feh --slideshow-delay 5 --auto-zoom --fullscreen
