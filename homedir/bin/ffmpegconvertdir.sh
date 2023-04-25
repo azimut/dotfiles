4553c9043df8806e62e57649a28538acc5f01324
azimut <azimut.github@protonmail.com>
2023-04-25 15:20:33 -0300

    ffmpegconvertdir: add notify counter

--- homedir/bin/ffmpegconvertdir.sh
+++ homedir/bin/ffmpegconvertdir.sh
@@ -22,19 +22,33 @@ set -euo pipefail
 SRC="$(realpath "${1}")"
 FILTERS="${2:-scale=960:-1}"
 
+info() {
+	notify-send -t "$((2 * 1000))" -- \
+		"$0" \
+		"<span color='#57dafd' font='20px'>${1}</span>"
+}
+
 # Create DST directories
 find "${SRC}" -type d | tail -n+2 |
 	while read -r dir; do
 		mkdir -vp ".${dir#${SRC}}"
 	done
 
+# Counters
+total="$(find "${SRC}" -type f \( -iname \*.mp4 -o -iname \*.mkv \) | wc -l)"
+i=0
+
 # Convert videos
 find "${SRC}" -type f \( -iname \*.mp4 -o -iname \*.mkv \) |
 	while read -r srcfile; do
 		dstfile=".${srcfile#${SRC}}"
+		info "${i}/${total}"
+		((++i))
 		[[ -f ${dstfile} ]] && {
 			echo "Skipping ${dstfile}..."
 			continue
 		}
 		ffmpeg -hide_banner -i "${srcfile}" -ac 1 -vf "${FILTERS}" "${dstfile}" </dev/null
 	done
+
+info "done!"
