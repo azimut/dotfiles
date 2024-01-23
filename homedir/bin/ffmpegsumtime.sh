4aa681e5ca35b59f15c29eb917cfee6ad8ead5f3
azimut <azimut.github@protonmail.com>
2024-01-23 20:52:13 -0300

    ffmpegsumtime.sh: minor format

--- homedir/bin/ffmpegsumtime.sh
+++ homedir/bin/ffmpegsumtime.sh
@@ -3,11 +3,7 @@
 # Description: sums the reproduction time of all .mp4 videos under
 
 sum() { paste -sd+ /dev/stdin | bc; }
-seconds_format() {
-	local seconds
-	seconds="$(</dev/stdin)"
-	date -d@"${seconds}" -u +%H:%M:%S
-}
+seconds_format() { date -d@"$(</dev/stdin)" -u +%H:%M:%S; }
 
 find . -xdev -name '*.mp4' | while read -r video; do
 	ffprobe -v quiet -show_streams -hide_banner -print_format json -i "${video}" |
