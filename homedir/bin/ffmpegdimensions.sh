78ae6b21b52dd7578a12910f270bcc57b5f113d7
azimut <azimut.github@protonmail.com>
2024-02-04 08:56:51 -0300

    ffmpeg: update while loop

--- homedir/bin/ffmpegdimensions.sh
+++ homedir/bin/ffmpegdimensions.sh
@@ -2,7 +2,8 @@
 
 # Description: show dimensions of all .mp4 videos under
 
-find . -name '*.mp4' | while read -r video; do
-	ffprobe -v quiet -show_streams -show_format -hide_banner -print_format json -i "${video}" |
-		jq -r ' . + { "video": .streams |  .[] | select(.width) } | "\(.video.width)x\(.video.height) - \(.format.filename)"'
-done
+find . -name '*.mp4' |
+	while read -r video; do
+		ffprobe -v quiet -show_streams -show_format -hide_banner -print_format json -i "${video}" |
+			jq -r ' . + { "video": .streams |  .[] | select(.width) } | "\(.video.width)x\(.video.height) - \(.format.filename)"'
+	done
--- homedir/bin/ffmpegsumtime.sh
+++ homedir/bin/ffmpegsumtime.sh
@@ -5,7 +5,8 @@
 sum() { paste -sd+ /dev/stdin | bc; }
 seconds_format() { date -d@"$(</dev/stdin)" -u +%H:%M:%S; }
 
-find . -xdev -name '*.mp4' | while read -r video; do
-	ffprobe -v quiet -show_format -hide_banner -print_format json -i "${video}" |
-		jq -r '.format.duration'
-done | sum | seconds_format
+find . -xdev -name '*.mp4' |
+	while read -r video; do
+		ffprobe -v quiet -show_format -hide_banner -print_format json -i "${video}" |
+			jq -r '.format.duration'
+	done | sum | seconds_format
