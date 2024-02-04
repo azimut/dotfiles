acceea1039a3028f639df23468307c1d1ea0e252
azimut <azimut.github@protonmail.com>
2024-02-04 15:58:56 -0300

    pdffromhtmldir.sh: add progress counter and resume feature

--- homedir/bin/ffmpegdimensions.sh
+++ homedir/bin/ffmpegdimensions.sh
@@ -5,5 +5,5 @@
 find . -name '*.mp4' |
 	while read -r video; do
 		ffprobe -v quiet -show_streams -show_format -hide_banner -print_format json -i "${video}" |
-			jq -r ' . + { "video": .streams |  .[] | select(.width) } | "\(.video.width)x\(.video.height) - \(.format.filename)"'
+			jq -r ' . + { "video": .streams | .[] | select(.width) } | "\(.video.width)x\(.video.height) - \(.format.filename)"'
 	done
--- homedir/bin/pdffromhtmldir.sh
+++ homedir/bin/pdffromhtmldir.sh
@@ -13,6 +13,12 @@ fetch() {
 		"${1}"
 }
 
+info() {
+	notify-send -t "$((5 * 1000))" -- \
+		"$0" \
+		"<span color='#57dafd' font='20px'>${1}</span>"
+}
+
 [[ $# -ne 1 ]] && {
 	echo "ERROR: missing directory argument"
 	exit 1
@@ -31,12 +37,24 @@ find "${SRC}" -mindepth 1 -type d |
 		mkdir -vp ".${dir#${SRC}}"
 	done
 
+# Counters
+total="$(find "${SRC}" -name '*.html' | wc -l)"
+i=0
+
 # Render .html into pdf
 find "${SRC}" -name '*.html' |
 	while read -r srcfile; do
 		dstfile=".${srcfile#"${SRC}"}"
+		dstfile="${dstfile%.html}.pdf"
 		fullpath="file://$(realpath "${srcfile}")"
+
+		((++i))
+		info "${i} of ${total}"
+		[[ -f ${dstfile} ]] && {
+			continue
+		}
+
 		fetch "${fullpath}" tmp.pdf
-		pdfcrop --margins '-5 -5 -5 -5' tmp.pdf "${dstfile%.html}.pdf"
+		pdfcrop --margins '-5 -5 -5 -5' tmp.pdf "${dstfile}"
 		rm -f tmp.pdf
 	done
