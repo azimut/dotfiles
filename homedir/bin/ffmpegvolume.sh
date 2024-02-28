f4f4930bf2463ce90467e79b46fdd506ef0919b5
azimut <azimut.github@protonmail.com>
2024-02-28 11:04:20 -0300

    ffmpegvolume: minor format

--- homedir/bin/ffmpegvolume.sh
+++ homedir/bin/ffmpegvolume.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ ! -s $1 ]]; then
-	echo "Description: returns the volume loudness of the passed file."
+	echo "Return the volume loudness of the passed file."
 	echo -e "Usage:\n\t\$ ${0##*/} <FILENAME>"
 	exit 1
 fi
 
 ffmpeg -i "$1" -af volumedetect -sn -dn -vn -f null /dev/null 2>&1 |
 	awk -v filename="$(basename "$1")" \
-		'/mean_volume/ { print $5, filename }'
+		'/mean_volume/ { printf "%-+.2f  %s\n", $5, filename }'
