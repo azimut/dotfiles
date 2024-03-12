a8d355800d3914af9ea28e85d8679795cbfcdf55
azimut <azimut.github@protonmail.com>
2024-03-12 08:39:23 -0300

    secrets update

--- homedir/bin/savesecrets.sh
+++ homedir/bin/savesecrets.sh
@@ -6,23 +6,31 @@ SECRETS=(
 	"${HOME}/.authinfo.gpg"
 	"${HOME}/.bash_secret"
 	"${HOME}/.erlang.cookie"
-	"${HOME}/.ssh/id_rsa"
-	"${HOME}/.ssh/id_rsa.pub"
 	"${HOME}/.psql_history"
 	"${HOME}/.zsh_history"
 	"${HOME}/.bash_history"
+	"${HOME}/gallery-dl.conf"
+	"${HOME}/projects"
 	"${HOME}/texts/ideas.org"
+	"${HOME}/.ssh/id_rsa"
+	"${HOME}/.ssh/id_rsa.pub"
 	"${HOME}/.newsboat/feeds/twitch-rss.secret"
 	"${HOME}/.newsboat/cache.db"
 	"${HOME}/.newsboat/history.search"
 	"${HOME}/.newsboat/history.cmdline"
+	"${HOME}/.config/exercism/user.json"
+	"${HOME}/.config/calibre"
 	/var/lib/bitlbee
 	/var/lib/znc
 )
 
-nDESTINATION="${HOME}/disk2/secrets"
-mkdir -p "${DESTINATION}"
+DESTINATION="${HOME}/prestaging"
+
+set -exuo pipefail
 
 for secret in "${SECRETS[@]}"; do
-	cp -r "${secret}" "${DESTINATION}"/
+	dirname="$(dirname "${secret}")"
+	dstdir="${DESTINATION}/${dirname#/}"
+	mkdir -p "${dstdir}"
+	sudo cp -a "${secret}" "${dstdir}"
 done
