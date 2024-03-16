689835563d0b206a61e281e1a74aa256582bf523
azimut <azimut.github@protonmail.com>
2024-03-16 16:10:16 -0300

    install: autoformat + remove relative "./" path

--- install-home.sh
+++ install-home.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd homedir/ && {
-    find . -type f -print0 |
-        while IFS= read -r -d '' f; do
-            mkdir -p "${HOME}/${f%/*}"
-            ln -sf "${PWD}/${f}" "${HOME}/${f}"
-        done
+	find . -type f -print0 |
+		while IFS= read -r -d '' f; do
+			mkdir -p "${HOME}/${f%/*}"
+			ln -sf "${PWD}/${f#./}" "${HOME}/${f#./}"
+		done
 }
