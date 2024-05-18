4e1fbf91f5bb0ad67d9a0aa63c40c3504f710a24
azimut <azimut.github@protonmail.com>
2024-05-18 08:54:25 -0300

    ffmpegdimensions.sh: refactor

--- homedir/bin/ffmpegdimensions.sh
+++ homedir/bin/ffmpegdimensions.sh
@@ -5,5 +5,6 @@
 find . -name '*.mp4' |
 	while read -r video; do
 		ffprobe -v quiet -show_streams -show_format -hide_banner -print_format json -i "${video}" |
-			jq -r ' . + { "video": .streams | .[] | select(.width) } | "\(.video.width)x\(.video.height) - \(.format.filename)"'
+			jq -r '(.streams[]|select(.width)) as $video
+                    | "\($video.width)x\($video.height) - \(.format.filename)"'
 	done
