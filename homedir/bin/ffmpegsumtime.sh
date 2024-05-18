4f80b49b29bc8f093cecc5abf73554947094f3cc
azimut <azimut.github@protonmail.com>
2024-05-18 15:49:50 -0300

    ffmpegsumtime.sh: refactor, replace aux functions by jq

--- homedir/bin/ffmpegsumtime.sh
+++ homedir/bin/ffmpegsumtime.sh
@@ -2,10 +2,7 @@
 
 # Description: sums the reproduction time of all .mp4 videos under
 
-sum() { paste -sd+ /dev/stdin | bc; }
-seconds_format() { date -d@"$(</dev/stdin)" -u +%H:%M:%S; }
-
 find . -xdev -name '*.mp4' -or -name '*.mp3' |
 	while read -r video; do
 		ffprobe -v quiet -show_format -hide_banner -print_format json -i "${video}"
-	done | jq -r '.format.duration' | sum | seconds_format
+	done | jq -sr 'map(.format.duration|tonumber) | add | strftime("%H:%M:%S")'
