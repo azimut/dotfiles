9bcefcc00fa9d8be35dfda91baa4e23bd21646c0
azimut <azimut.github@protonmail.com>
2024-05-27 14:31:08 -0300

    pinp.sh: use only awk for integer multiplication

--- homedir/bin/pinp.sh
+++ homedir/bin/pinp.sh
@@ -5,8 +5,7 @@
 
 set -euo pipefail
 
-mul() { echo "$(</dev/stdin) * ${1}" | bc -l; }
-round() { awk '{ print(int($1)) }' </dev/stdin; }
+imul() { awk -v a="$1" -v b="$2" 'BEGIN { print(int(a*b)) }'; }
 
 getTargetID() {
 	xdotool search --name "picture-in-picture" || xdotool search --onlyvisible --class mpv
@@ -29,14 +28,14 @@ ID="$(getTargetID)"
 read -r -a SCREEN_DIMENSIONS < <(getScreenDimensions)
 read -r -a CURRENT_DIMENSIONS < <(getWindowDimensions ${ID})
 
-TARGET_WIDTH="$(echo ${CURRENT_DIMENSIONS[0]} | mul ${MULTIPLIER} | round)"
-TARGET_HEIGHT="$(echo ${CURRENT_DIMENSIONS[1]} | mul ${MULTIPLIER} | round)"
+TARGET_WIDTH="$(imul "${CURRENT_DIMENSIONS[0]}" "${MULTIPLIER}")"
+TARGET_HEIGHT="$(imul "${CURRENT_DIMENSIONS[1]}" "${MULTIPLIER}")"
 
 printf "%sx%s -> %sx%s\n" \
 	${CURRENT_DIMENSIONS[0]} ${CURRENT_DIMENSIONS[1]} \
 	${TARGET_WIDTH} ${TARGET_HEIGHT}
-xdotool windowsize ${ID} "${TARGET_WIDTH}" "${TARGET_HEIGHT}"
 
+xdotool windowsize ${ID} "${TARGET_WIDTH}" "${TARGET_HEIGHT}"
 xdotool windowmove ${ID} \
 	$((SCREEN_DIMENSIONS[0] - TARGET_WIDTH)) \
 	$((0 + BAR_OFFSET))
