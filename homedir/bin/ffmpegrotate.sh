67eb33bedcee8e581f6510d0e1f35c04019bd52c
azimut <azimut.github@protonmail.com>
2024-05-29 12:59:03 -0300

    ffmpegrotate.sh: use ipickme to pick a rotation

--- homedir/bin/ffmpegrotate.sh
+++ homedir/bin/ffmpegrotate.sh
@@ -4,38 +4,65 @@
 
 set -euo pipefail
 
-ferror() {
+err() {
 	echo "ERROR: $1" >&2
 	exit 1
 }
-
-[[ $# -ne 2 ]] && {
-	ferror "needs two params, 1st is file, 2nd rotation in degrees"
-	return 1
+usage() {
+	echo -e "ERROR: $1\nUSAGE:\n\t$(basename $0) VIDEO" >&2
+	exit 1
+}
+round() { awk '{ print(int($0)) }'; }
+timestamp() { date -u --date=@"${1}" +%H:%M:%S; }
+seconds() {
+	ffprobe -loglevel error -hide_banner -of csv=p=0 -show_entries format=duration "$1" | round
 }
 
-[[ ! -f $1 ]] && {
-	ferror "file does not exists"
-	return 1
-}
+[[ $# -ne 1 ]] && usage "missing argument"
+[[ ! -f $1 ]] && usage "file does not exists"
 
 video="$1"
+declare -A rotations=(
+	[0]="copy"
+	[90]="transpose=1"
+	[180]="transpose=2,transpose=2"
+	[270]="transpose=2"
+)
+
+# 0: generate rotated thumbnails
+seconds=$((RANDOM % $(seconds "${video}") + 1))
+for rot in "${!rotations[@]}"; do
+	ffmpeg -y -hide_banner -loglevel error \
+		-ss "$(timestamp ${seconds})" \
+		-i "${video}" \
+		-vf "${rotations[$rot]}" \
+		-frames:v 1 \
+		"extract_tmp_${rot}.jpg"
+done
+
+# 1: pick rotation
+rot=""
+img="$(ipickme extract_tmp*.jpg)"
+case "${img}" in
+*_0.jpg) err "we choose to not rotating anything..." ;;
+*jpg)
+	rot="${img##*tmp_}"
+	rot="${rot%.jpg}"
+	;;
+*) err "nothing picked..." ;;
+esac
+rm -f extract_tmp*.jpg
+
+# 2: generate video
 filename="$(basename "${video}")"
 extension="${filename##*.}"
 filename="${filename%.*}"
-opts=""
-
-case $2 in
-90) opts="transpose=1" ;;
-90f) opts="transpose=3" ;;
-180) opts="transpose=2,transpose=2" ;;
-270) opts="transpose=2" ;;
-270f) opts="transpose=0" ;;
-*) ferror "should have been a value among 90, 180 or 270" ;;
-esac
-
-ffmpeg -hide_banner \
+output_file="${filename}_${rot}.${extension}"
+ffmpeg -y -hide_banner -loglevel error \
 	-i "${video}" \
-	-vf "${opts}" \
-	-codec:a copy \
-	"${filename}_$2.${extension}"
+	-metadata:s:v rotate="-${rot}" \
+	-codec copy \
+	"${output_file}"
+
+rm -vf "${video}"
+echo "new \'${output_file}\'"
