4c50c47924939e9ac0153c7476490f3d6be6f364
azimut <azimut.github@protonmail.com>
2024-06-01 17:23:52 -0300

    ffmpegsumtime.sh: show increasing sum

--- homedir/bin/ffmpegsumtime.sh
+++ homedir/bin/ffmpegsumtime.sh
@@ -2,7 +2,9 @@
 
 # Description: sums the reproduction time of all .mp4 videos under
 
-find . -xdev -name '*.mp4' -or -name '*.mp3' |
+find . -name '*.mp4' -or -name '*.mp3' |
 	while read -r video; do
-		ffprobe -v quiet -show_format -hide_banner -print_format json -i "${video}"
-	done | jq -sr 'map(.format.duration|tonumber) | add | strftime("%H:%M:%S")'
+		ffprobe -v quiet -show_format -hide_banner -print_format json -i "${video}" |
+			jq -r '.format.duration|tonumber'
+	done |
+	awk '{ t+=$1; printf("\033[10D"); printf("%s", strftime("%H:%M:%S", t, 1)); } END { print "" }'
