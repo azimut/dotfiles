eb66d3651c85f161cc8ae3dffb1dab97338b69f0
azimut <azimut.github@protonmail.com>
2024-06-01 18:37:11 -0300

    ffmpegsumtime.sh: refactor, move jq spawn outside loop

--- homedir/bin/ffmpegsumtime.sh
+++ homedir/bin/ffmpegsumtime.sh
@@ -4,7 +4,7 @@
 
 find . -name '*.mp4' -or -name '*.mp3' |
 	while read -r video; do
-		ffprobe -v quiet -show_format -hide_banner -print_format json -i "${video}" |
-			jq -r '.format.duration|tonumber'
+		ffprobe -v quiet -show_format -hide_banner -print_format json -i "${video}"
 	done |
+	jq -r --unbuffered '.format.duration|tonumber' |
 	awk '{ t+=$1; printf("\033[10D"); printf("%s", strftime("%H:%M:%S", t, 1)); } END { print "" }'
