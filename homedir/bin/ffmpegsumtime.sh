ec48ae5bf06d317da26ff515b1ea10c5ecbf3fb5
azimut <azimut.github@protonmail.com>
2024-06-05 20:08:22 -0300

    ffmpegsumtime.sh: feat: add other filenames, fix: print less ffprobe

--- homedir/bin/ffmpegsumtime.sh
+++ homedir/bin/ffmpegsumtime.sh
@@ -2,8 +2,8 @@
 
 # Description: sums the reproduction time of all .mp4 videos under
 
-find . -name '*.mp4' -or -name '*.mp3' |
+find . -iname '*.mp4' -or -iname '*.mp3' -or -iname '*.webm' |
 	while read -r video; do
-		ffprobe -v error -show_entries format=duration -of default=nokey=1 "${video}"
+		ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${video}"
 	done |
 	awk '{ t+=$1; printf("\033[10D"); printf("%s", strftime("%H:%M:%S", t, 1)); } END { print "" }'
