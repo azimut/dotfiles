e4e0410d0f7b664b9d24d571fc9867d4e4f9242b
azimut <azimut.github@protonmail.com>
2024-06-06 02:45:15 -0300

    ffmpegsumtime.sh: feat: print nothing, when nothing is found

--- homedir/bin/ffmpegsumtime.sh
+++ homedir/bin/ffmpegsumtime.sh
@@ -6,4 +6,4 @@ find . -regextype egrep -iregex '.*(mp4|mp3|webm)' |
 	while read -r video; do
 		ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${video}"
 	done |
-	awk '{ t+=$1; printf("\033[10D%s", strftime("%H:%M:%S", t, 1)); } END { print "" }'
+	awk '{ t+=$1; printf("\033[10D%s", strftime("%H:%M:%S", t, 1)); } END { if(t) print("") }'
