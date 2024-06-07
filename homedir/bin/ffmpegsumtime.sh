87e44986a1ebe7105a45beb2246ca59c915c17b8
azimut <azimut.github@protonmail.com>
2024-06-07 12:28:22 -0300

    ffmpegsumtime.sh: while -> xargs -P2
    a pinch of parallelism
    
    should(tm) be safe under 8092 chars per command
    https://mywiki.wooledge.org/BashPitfalls#Non-atomic_writes_with_xargs_-P
    
    -r for not running on no input

--- homedir/bin/ffmpegsumtime.sh
+++ homedir/bin/ffmpegsumtime.sh
@@ -2,8 +2,7 @@
 
 # Description: sums up the reproduction time of all media under CWD
 
-find . -regextype egrep -iregex '.*(mp4|mp3|webm)' |
-	while read -r video; do
-		ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${video}"
-	done |
+find . -regextype egrep -iregex '.*(mp4|mp3|webm)' -print0 |
+	xargs -n1 -0 -r -P2 \
+		ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 |
 	awk '{ t+=$1; printf("\033[10D%s", strftime("%H:%M:%S", t, 1)); } END { if(t) print("") }'
