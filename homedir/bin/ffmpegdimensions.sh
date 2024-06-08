7b00c076f619534d794f224e620021a9acf0d42c
azimut <azimut.github@protonmail.com>
2024-06-08 16:05:55 -0300

    ffmpegdimensions.sh: feat: parallel work, clear columns

--- homedir/bin/ffmpegdimensions.sh
+++ homedir/bin/ffmpegdimensions.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Description: show dimensions of all .mp4 videos under
 
-find . -name '*.mp4' |
-	while read -r video; do
-		ffprobe -v quiet -show_streams -show_format -hide_banner -print_format json -i "${video}" |
-			jq -r '(.streams[]|select(.width)) as $video
-                    | "\($video.width)x\($video.height) - \(.format.filename)"'
-	done
+getdimensions() {
+	video="$1"
+	dimensions="$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "${video}")"
+	printf '%5dx%-4d\t'"'"'%s'"'"'\n' \
+		"${dimensions%x*}" \
+		"${dimensions#*x}" \
+		"${video#*/}"
+}
+export -f getdimensions
+
+find . -name '*.mp4' -print0 | sort -zn |
+	xargs -I{} -n1 -0 -P2 -r \
+		bash -c 'getdimensions "{}"'
