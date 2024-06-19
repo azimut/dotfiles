67e2c6549c1f043722fdbf23fbfec83492a6486e
azimut <azimut.github@protonmail.com>
2024-06-19 04:16:23 -0300

    waybackurl.sh: -s for silent curl

--- homedir/bin/waybackurl.sh
+++ homedir/bin/waybackurl.sh
@@ -8,5 +8,5 @@
 
 [ $# -ne 1 ] && echo "Error: needs an url parameter." && exit 1
 
-curl "http://archive.org/wayback/available?url=${1}&timestamp=19960101" |
+curl -s "http://archive.org/wayback/available?url=${1}&timestamp=19960101" |
 	jq -r '.archived_snapshots.closest | .status + " " + .url'
