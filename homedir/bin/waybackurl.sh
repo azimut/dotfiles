d93e9d2fc3fbc193655283573efa64c077096d20
azimut <azimut.github@protonmail.com>
2024-06-19 18:32:01 -0300

    waybackurl.sh: cleanup fragment

--- homedir/bin/waybackurl.sh
+++ homedir/bin/waybackurl.sh
@@ -2,11 +2,8 @@
 
 # Description: Returns the oldest url available
 # Documentation: https://archive.org/help/wayback_api.php
-#
-# TODO:
-# - Cleanup fragment from url (#)
 
 [ $# -ne 1 ] && echo "Error: needs an url parameter." && exit 1
 
-curl -s "http://archive.org/wayback/available?url=${1}&timestamp=19960101" |
+curl -s "http://archive.org/wayback/available?url=${1%#*}&timestamp=19960101" |
 	jq -r '.archived_snapshots.closest | .status + " " + .url'
